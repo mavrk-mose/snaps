62226ff07d7f656b623ceba191f15a84a40d3145
Moses Kippe <moseskippe@ellipsis.co.tz>
2022-10-24 10:03:23 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="max-w-2xl mx-auto py-16 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      .grid.grid-cols-1.gap-y-10.sm:grid-cols-2.gap-x-6.lg:
+    </div>
+  )
+}
+
+export default App
