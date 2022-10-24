c8aee2f76e16fd76a203ce1a4df9b92693aaed8c
mavrk-mose <kippemoses25@gmail.com>
2022-10-24 17:05:01 +0300

    filtered API query parameters

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [topAnime, setTopAnime] = useState([]);
 
   useEffect(() => {
-    axios('https://api.jikan.moe/v4/top/anime')
+    axios('https://api.jikan.moe/v4/top/anime?filter=upcoming')
     .then(response =>{
     setTopAnime(prev => {
       prev = response.data.data
@@ -15,12 +15,13 @@ function App() {
     });
   })
   }, []);
+  console.log(topAnime)
   return (
-    <div className="max-w-2xl mx-auto py-16 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+    <div className="mx-auto max-w-2xl py-16 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {topAnime.map((anime, index) => {
             return (
-              <BlurImage key={index} trailer={anime.trailer.embed_url}image={anime.images.webp.image_url} title={anime.title} aired={anime.aired.string}/>
+              <BlurImage key={index} trailer={anime.trailer.url} image={anime.images.webp.image_url} title={anime.title} aired={anime.aired.string}/>
             )
           })}
         </div>
