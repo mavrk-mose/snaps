20cdf70183341d6e47649bd316fcfd9fbd418b32
mavrk-mose <kippemoses25@gmail.com>
2022-10-24 15:11:17 +0300

    added blur effect when loading

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,5 +1,12 @@
+import { useState } from "react";
+
+function cn(...classes) {
+    return classes.filter(Boolean).join(' ');
+  }
+  
 
 function BlurImage({image}) {
+    const [isLoading, setLoading] = useState(true);
   return (
     <a href="#" className="group">
       <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
@@ -8,9 +15,13 @@ function BlurImage({image}) {
             src={image} 
             layout="fill"
             objectfit = "cover"
-            className='group-hover:opacity-75 duration-700 ease-in-out'   
+            className={cn('group-hover:opacity-75 duration-700 ease-in-out',
+            isLoading
+              ? 'grayscale blur-2xl scale-110'
+              : 'grayscale-0 blur-0 scale-100'
+          )}
+          onLoad={() => setLoading(false)}
           />
-        
       </div>
       <h3 className="mt-4 text-sm text-gray-700">ger</h3>
       <p className="mt-1 text-lg font-medium text-gray-900">@leeerob</p>
