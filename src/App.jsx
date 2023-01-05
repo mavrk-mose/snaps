9353448c5a2aaeb42803e590556b1f8d3df2ead3
mavrk-mose <kippemoses25@gmail.com>
2023-01-05 16:52:26 +0300

    added score and a few tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import BlurImage from './components/Image';
-import axios from "axios";
+import React from 'react';
+import Home from './pages/Home';
+import Description from './pages/Description';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [topAnime, setTopAnime] = useState([]);
-
-useEffect(() => {
-    axios('https://api.jikan.moe/v4/top/anime?filter=movies')
-    .then(response =>{
-    setTopAnime(prev => {
-      prev = response.data.data
-      return prev
-    });
-  })
-  }, []);
 
   return (
-    <div className="mx-auto max-w-2xl py-16 sm:py-24 px-8 sm:px-8 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {topAnime.map((anime, index) => {
-            return (
-              <BlurImage key={index} trailer={anime.trailer.url} image={anime.images.webp.image_url} title={anime.title} aired={anime.aired.string}/>
-            )
-          })}
-        </div>
-    </div>
+   <Router>
+    <Routes>
+      <Route exact path='/' element={<Home/>} />
+      <Route path='/Description' element={<Description/>} />
+    </Routes>
+   </Router>
   )
 }
 
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,31 +1,32 @@
 import { useState } from "react";
+import { StarRounded } from "@material-ui/icons";
 
 function cn(...classes) {
     return classes.filter(Boolean).join(' ');
   }
   
-function BlurImage({image, title, aired, trailer}) {
+function BlurImage({image, title, aired, score, trailer}) {
     const [isLoading, setLoading] = useState(true);
 
     
   return (
-    <a href={trailer} className="group">
-      <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-            <img
-            alt="Anime Image"
-            src={image} 
-            layout="fill"
-            objectfit = "cover"
-            className={cn('group-hover:opacity-75 duration-700 ease-in-out',
-            isLoading
-              ? 'grayscale blur-2xl scale-110'
-              : 'grayscale-0 blur-0 scale-100'
-          )}
-          onLoad={() => setLoading(false)}
-          />
-      </div>
-      <h3 className="mt-4 text-sm text-yellow-700">{title}</h3>
-      <p className="mt-1 text-lg font-small text-yellow-900">{aired}</p>
+    <a href={trailer} title="watch trailer">
+        <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+              <img
+                alt="Anime Image"
+                src={image} 
+                layout="fill"
+                objectfit = "cover"
+                className={cn('group-hover:opacity-75 duration-700 ease-in-out',
+                isLoading ? 'grayscale blur-2xl scale-110' : 'grayscale-0 blur-0 scale-100')}
+              onLoad={() => setLoading(false)}
+              />    
+        </div>
+          <h3 className="mt-4 text-sm text-yellow-700">{title}</h3>
+        <div className="flex text-yellow-700">
+          <StarRounded/> {score}
+        </div>
+        <p className="mt-1 text-lg font-small text-yellow-900">{aired}</p>
     </a>
   );
 }
